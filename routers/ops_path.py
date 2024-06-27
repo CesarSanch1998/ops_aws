022a4d0cd62b7d5b16bd2eed8afe473ee8a803ea
CesarSanch1998 <cs96548@gmail.com>
2024-06-27 10:37:22 -0400

    print data client

--- routers/ops_path.py
+++ routers/ops_path.py
@@ -11,7 +11,9 @@ async def ops_clients():
 async def ops_clients(request: Request):
     body = await request.body()  # Obtener el cuerpo de la solicitud
     data = json.loads(body) 
+    for client in data["clients"]:
+        
     # with open(f"datosdelodoo.json", "a") as archivo_json:
     #     json.dump(data, archivo_json, indent=4)
-    print(data)
+        print(client)
     return data
