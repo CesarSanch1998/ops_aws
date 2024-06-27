864c917251de4f0e194a8fe1228ccddcee14db5d
CesarSanch1998 <cs96548@gmail.com>
2024-06-27 09:59:20 -0400

    json save

--- routers/ops_path.py
+++ routers/ops_path.py
@@ -11,5 +11,6 @@ async def ops_clients():
 async def ops_clients(request: Request):
     body = await request.body()  # Obtener el cuerpo de la solicitud
     data = json.loads(body) 
-    
+    with open(f"datosdelodoo.json", "a") as archivo_json:
+        json.dump(data, archivo_json, indent=4)
     return data
