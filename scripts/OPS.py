021f6eac27eadcba290732210413aabd170ccea9
CesarSanch1998 <cs96548@gmail.com>
2024-06-27 14:21:50 -0400

    fix commit 3

--- scripts/OPS.py
+++ scripts/OPS.py
@@ -34,8 +34,8 @@ def client_operate(data):
 
 def accion(db_data):
     fsp_buscado = f"{str(db_data.fsp)}"
-    for clave, oid_puerto in map_ports.items():
-        if oid_puerto == fsp_buscado:
+    for oid_puerto, fsp_puerto in map_ports.items():
+        if fsp_puerto == fsp_buscado:
             get_serial = Get(olt_devices[str(db_data.olt)],os.environ['SNMP_READ'],f"1.3.6.1.4.1.2011.6.128.1.1.2.43.1.3.{oid_puerto}.{db_data.onu_id}")
             print(get_serial)
             print(f"1.3.6.1.4.1.2011.6.128.1.1.2.43.1.3.{oid_puerto}.{db_data.onu_id}")
